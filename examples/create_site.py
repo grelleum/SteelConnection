3928b1cc36c251529dfffff48e9c753189a886b3
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-04 00:27:42 -0400

    Added create_site example.

---
+++ examples/create_site.py
@@ -0,0 +1,31 @@
+import steelconnection
+
+# Change the below values to match the realm and org as seen in the URL for your SteelConnect Manager.
+# for example:  https://realm.riverbed.cc/admin/TestLab
+scm_name = 'realm.riverbed.cc'
+org_name = 'TestLab'   
+
+# Details for the site to be created:
+new_site = {
+    'name': 'NYC',
+    'city': 'New York',
+    'country': 'US',
+    'longname': 'New York test lab.',
+}
+
+# Initialize the steelconnection object.
+sconnect = steelconnection.Config(scm_name)
+
+# Get the org ID for your organization.
+org_id = sconnect.lookup.org(org_name)
+print('Org name: {},  org_id: {}'.format(org_name, org_id))
+
+# API resource for posting.
+resource = '/org/{}/sites'.format(org_id)
+
+# Make the post request.
+response = sconnect.post(resource, data=new_site)
+
+# Display result.
+print('Response:', response.status_code, response.reason)
+print(response.data)
