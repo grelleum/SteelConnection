1be67cf845b7c016cf3544a18b6a4cd1cc06a075
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-05 10:45:47 -0400

    Cleaned up code.

--- examples/get_ports.py
+++ examples/get_ports.py
@@ -1,8 +1,6 @@
 """List port information for a SteelConnect appliance."""
 
-
 import steelconnection
-
 
 realm = steelconnection.get_input('Please enter your SteelConnect Realm: ')
 sconnect = steelconnection.SConAPI(realm)
@@ -11,7 +9,6 @@ appliance = steelconnection.get_input('Please enter the appliance serial number:
 node_id = sconnect.lookup.nodeid(appliance)
 
 ports = sconnect.config.get(f'node/{node_id}/ports').data
-
 
 print('\nPort ID \tifname \tLink \tSpeed \tDuplex')
 print('------- \t------ \t---- \t----- \t------')
