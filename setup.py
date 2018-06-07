4f24aa6ed6931725f936aafa03d32b4d1393dbf7
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-07 12:05:29 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+name = 'steelconnection'
+version = '0.7.0'
+description = 'Simplify REST API access to Riverbed SteelConnect.'
+url = 'https://github.com/grelleum/SteelConnection'
+author = 'Greg Mueller'
+author_email = 'greg@grelleum.com'
+
+classifiers=(
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+),
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=author_email,
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=url,
+    packages=setuptools.find_packages(),
+    classifiers=classifiers
+)
