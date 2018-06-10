93e2d97b4aba75b0d6c6ed7065244c269777950c
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-10 10:06:42 -0400

    Import input tools from input_tools.

---
+++ steelconnection/__init__.py
@@ -0,0 +1,15 @@
+from requests import HTTPError
+
+from steelconnection.steelconnection import SConAPI
+from steelconnection.input_tools import get_input
+from steelconnection.input_tools import get_username
+from steelconnection.input_tools import get_password
+
+__version__ = '0.7.0'
+__all__ = (
+    SConAPI,
+    HTTPError,
+    get_input,
+    get_username,
+    get_password,
+)
