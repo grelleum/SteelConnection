5fd9b247c14e2a4d1d44dd393723beb87e1d2db5
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-10 09:55:44 -0400

    separated input tools from steelconnection

---
+++ steelconnection/input_tools.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+"""SteelConnection.input_tools
+
+Convienience functions for getting interactive input from terminal.
+"""
+
+
+from __future__ import print_function
+import getpass
+import sys
+
+from requests import HTTPError
+
+def get_input(prompt=''):
+    """Get input in a Python 2/3 compatible way."""
+    try:
+        data = raw_input(prompt)
+    except NameError:
+        data = input(prompt)
+    finally:
+        return data
+
+
+def get_username(prompt=''):
+    return get_input('Enter username: ')
+
+
+def get_password(prompt=None, password=None):
+    """Get password from terminal with discretion."""
+    prompt = 'Enter password: ' if prompt is None else prompt
+    while not password:
+        verify = False
+        while password != verify:
+            if verify:
+                print('Passwords do not match. Try again', file=sys.stderr)
+            password = getpass.getpass(prompt)
+            verify = getpass.getpass('Retype password: ')
+    return password
