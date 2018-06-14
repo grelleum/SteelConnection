d0da83135fb278eb47d3fd597206e8a0c8fdc868
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-14 18:44:52 -0400

    change to exception class

--- steelconnection/__init__.py
+++ steelconnection/__init__.py
@@ -2,17 +2,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from requests import HTTPError
-
 from steelconnection.steelconnection import SConAPI
+from steelconnection.steelconnection import SConError
 from steelconnection.input_tools import get_input
 from steelconnection.input_tools import get_username
 from steelconnection.input_tools import get_password
 
-__version__ = '0.8.2'
+__version__ = '0.8.3'
 __all__ = (
     'SConAPI',
-    'HTTPError',
+    'SConError',
     'get_input',
     'get_username',
     'get_password',
