ee18da9b85408b35021d73481c13e3454974a627
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-15 13:12:54 -0400

    Can now run directly with -m to get version and url.

---
+++ steelconnection/__main__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import steelconnection.version
+
+home = 'https://pypi.org/project/steelconnection/'
+
+if __name__ == '__main__':
+    print('SteelConnection version:', steelconnection.version.__version__)
+    print('Documentation available at', home)
