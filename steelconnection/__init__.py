2a505d250746f74bb35b06933a86517009386871
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-16 08:28:49 -0400

    import logging

--- steelconnection/__init__.py
+++ steelconnection/__init__.py
@@ -7,10 +7,13 @@ from steelconnection.steelconnection import SConAPI
 from steelconnection.input_tools import get_input
 from steelconnection.input_tools import get_username
 from steelconnection.input_tools import get_password
+
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
 import steelconnection.version
-
-
 __version__ = steelconnection.version.__version__
+
 __all__ = (
     'SConAPI',
     'get_input',
