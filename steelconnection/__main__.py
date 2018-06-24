f3f93c362efb63d291943c6c35c084fceca90365
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-24 03:03:05 -0400

    fixed version in __main__

--- steelconnection/__main__.py
+++ steelconnection/__main__.py
@@ -2,10 +2,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import steelconnection.version
+from .__version__ import __url__, __version__
 
-home = 'https://pypi.org/project/steelconnection/'
 
 if __name__ == '__main__':
-    print('SteelConnection version:', steelconnection.version.__version__)
-    print('Documentation available at', home)
+    print('SteelConnection version:', __version__)
+    print('Documentation available at', __url__)
