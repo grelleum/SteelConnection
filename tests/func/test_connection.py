e9bd069cc7b24b9d5f7a2e225eb3f658d6f840b4
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-26 10:27:53 -0400

    test against various controller versions

---
+++ tests/func/test_connection.py
@@ -0,0 +1,24 @@
+import steelconnection
+
+from PRIVATE import REALM_ADMIN, ORG_ADMIN, PASSWORD
+from PRIVATE import REALM_2_8, REALM_2_9, REALM_2_10, REALM_2_11
+
+
+def test_create_object_2_8():
+    sc = steelconnection.SConAPI(REALM_2_8, REALM_ADMIN, PASSWORD)
+    assert isinstance(sc, steelconnection.SConAPI)
+
+
+def test_create_object_2_9():
+    sc = steelconnection.SConAPI(REALM_2_9, REALM_ADMIN, PASSWORD)
+    assert isinstance(sc, steelconnection.SConAPI)
+
+
+def test_create_object_2_10():
+    sc = steelconnection.SConAPI(REALM_2_10, REALM_ADMIN, PASSWORD)
+    assert isinstance(sc, steelconnection.SConAPI)
+
+
+def test_create_object_2_11():
+    sc = steelconnection.SConAPI(REALM_2_11, REALM_ADMIN, PASSWORD)
+    assert isinstance(sc, steelconnection.SConAPI)
