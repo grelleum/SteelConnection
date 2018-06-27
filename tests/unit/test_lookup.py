b194f396584fbff10049b3c2ccfe08755ffb1cea
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-27 01:28:19 -0400

    modified to use monkey patching

--- tests/unit/test_lookup.py
+++ tests/unit/test_lookup.py
@@ -1,49 +1,44 @@
 # coding: utf-8
 
+import requests
 import steelconnection
-
-from PRIVATE import REALM_ADMIN, ORG_ADMIN, PASSWORD
-from PRIVATE import REALM_2_8, REALM_2_9, REALM_2_10, REALM_2_11
+import PATCH
 
 
-def test_lookup_lookup():
-    sc = steelconnection.SConAPI(REALM_2_10, REALM_ADMIN, PASSWORD)
-    items = sc.get('orgs')
-    items = (item for item in items if 'name' in item)
-    item = next(items)
+def test_lookup_lookup(monkeypatch):
+    monkeypatch.setattr(requests, 'Session', PATCH.Fake_Session)
+    sc = steelconnection.SConAPI('some.realm')
+    item = sc.get('orgs')[0]
     key = item['name']
     key_id = item['id']
     result = sc.lookup._lookup(domain='orgs', value=key, key='name')
     assert result == (key_id, item)
 
 
-def test_lookup_node():
-    sc = steelconnection.SConAPI(REALM_2_10, REALM_ADMIN, PASSWORD)
-    items = sc.get('nodes')
-    items = (item for item in items if 'serial' in item)
-    item = next(items)
+def test_lookup_node(monkeypatch):
+    monkeypatch.setattr(requests, 'Session', PATCH.Fake_Session)
+    sc = steelconnection.SConAPI('some.realm')
+    item = sc.get('nodes')[0]
     key = item['serial']
     key_id = item['id']
     result = sc.lookup.node(key)
     assert result == (key_id, item)
 
 
-def test_lookup_org():
-    sc = steelconnection.SConAPI(REALM_2_10, REALM_ADMIN, PASSWORD)
-    items = sc.get('orgs')
-    items = (item for item in items if 'name' in item)
-    item = next(items)
+def test_lookup_org(monkeypatch):
+    monkeypatch.setattr(requests, 'Session', PATCH.Fake_Session)
+    sc = steelconnection.SConAPI('some.realm')
+    item = sc.get('orgs')[0]
     key = item['name']
     key_id = item['id']
     result = sc.lookup.org(key)
     assert result == (key_id, item)
 
 
-def test_lookup_site():
-    sc = steelconnection.SConAPI(REALM_2_10, REALM_ADMIN, PASSWORD)
-    items = sc.get('sites')
-    items = (item for item in items if 'name' in item and 'org' in item)
-    item = next(items)
+def test_lookup_site(monkeypatch):
+    monkeypatch.setattr(requests, 'Session', PATCH.Fake_Session)
+    sc = steelconnection.SConAPI('some.realm')
+    item = sc.get('sites')[0]
     key = item['name']
     key_id = item['id']
     org_id = item['org']
