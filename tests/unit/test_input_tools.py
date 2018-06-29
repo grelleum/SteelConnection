8a61405c9847c166f9bc837fae15baa8d24e83ea
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-29 00:16:47 -0400

    fix monkey patchin of getpass

--- tests/unit/test_input_tools.py
+++ tests/unit/test_input_tools.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import getpass
 import sys
 from steelconnection import input_tools
 
@@ -23,18 +24,12 @@ def test_get_username(monkeypatch):
 
 
 def test_get_password(monkeypatch):
-    if sys.version_info.major < 3:
-        monkeypatch.setattr('__builtin__.raw_input', lambda x: 'SteelConnect')
-    else:
-        monkeypatch.setattr('builtins.input', lambda x: 'SteelConnect')
-    name = input_tools.get_password()
-    assert name == 'SteelConnect'
+    monkeypatch.setattr('getpass.getpass', lambda x: 'mypassword')
+    password = input_tools.get_password()
+    assert password == 'mypassword'
 
 
 def test_get_password_once(monkeypatch):
-    if sys.version_info.major < 3:
-        monkeypatch.setattr('__builtin__.raw_input', lambda x: 'SteelConnect')
-    else:
-        monkeypatch.setattr('builtins.input', lambda x: 'SteelConnect')
-    name = input_tools.get_password_once()
-    assert name == 'SteelConnect'
+    monkeypatch.setattr('getpass.getpass', lambda x: 'mypassword')
+    password = input_tools.get_password_once()
+    assert password == 'mypassword'
