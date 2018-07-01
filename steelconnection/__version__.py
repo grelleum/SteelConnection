28a08a9502de611879edde0010c634d4de32c95c
Greg Mueller <greg.p.mueller@gmail.com>
2018-06-30 22:46:45 -0400

    version bump

--- steelconnection/__version__.py
+++ steelconnection/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'steelconnection'
 __description__ = 'Simplify access to the Riverbed SteelConnect REST API.'
 __url__ = 'https://github.com/grelleum/SteelConnection'
-__version__ = '0.9.9'
+__version__ = '0.9.8.2'
 __author__ = 'Greg Mueller'
 __author_email__ = 'steelconnection@grelleum.com'
 __license__ = 'MIT'
