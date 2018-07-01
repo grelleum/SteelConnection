52a4266d6e641774615ed1789f589955cf9ee41e
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-01 15:22:59 -0400

    auto-generate __version__.py

--- setup.py
+++ setup.py
@@ -1,59 +1,69 @@
 from setuptools import setup
-
-# Problem was that I was importing version from steelconnection as her:
-# from steelconnection.__version__ import __version__
-# this caused pip to try to import steelconnection before installing it
-# and it has a dependency on requests that may not yet have been installed.
-#
-# solution is to have setup.py build the __version__ file,
-# such that setup.py is the single source of truth.
-#
-# Here is the install failing when the dependency is not installed:
-#
-# Looking in indexes: https://test.pypi.org/simple/
-# Collecting steelconnection
-#   Downloading https://test-files.pythonhosted.org/packages/0d/3b/323931e1d4379d193be02475497250c40c611eee85b0d51d910d2ef803de/steelconnection-0.9.9.1.tar.gz
-#     Complete output from command python setup.py egg_info:
-#     Traceback (most recent call last):
-#       File "<string>", line 1, in <module>
-#       File "/tmp/pip-install-bqu7w8dh/steelconnection/setup.py", line 2, in <module>
-#         from steelconnection.__version__ import __version__
-#       File "/tmp/pip-install-bqu7w8dh/steelconnection/steelconnection/__init__.py", line 20, in <module>
-#         from .steelconnection import SConAPI
-#       File "/tmp/pip-install-bqu7w8dh/steelconnection/steelconnection/steelconnection.py", line 29, in <module>
-#         import requests
-#     ModuleNotFoundError: No module named 'requests'
+import re
 
 
-__version__ = '0.9.9.2'
+__title__ = 'steelconnection'
+__description__ = 'Simplify access to the Riverbed SteelConnect REST API.'
+__version__ = '0.9.10'
+__author__ = 'Greg Mueller'
+__author_email__ = 'steelconnection@grelleum.com'
+__copyright__ = 'Copyright 2018 Greg Mueller'
+__license__ = 'MIT'
+__url__ = 'https://github.com/grelleum/SteelConnection'
+
+content_type = 'text/markdown'
+install_requires = ['requests>=2.12.1']
+keywords = ['SteelConnect', 'REST', 'API', 'Riverbed', 'Grelleum']
+packages = ['steelconnection']
+classifiers = [
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+]
+
+with open(__title__ + '/__version__.py', 'wt') as f:
+    text = "{0} = '{1}'\n"
+    f.write(text.format('__title__', __title__))
+    f.write(text.format('__description__', __description__))
+    f.write(text.format('__version__', __version__))
+    f.write(text.format('__author__', __author__))
+    f.write(text.format('__author_email__', __author_email__))
+    f.write(text.format('__copyright__', __copyright__))
+    f.write(text.format('__license__', __license__))
+    f.write(text.format('__url__', __url__))
+
+
+with open('README.md', 'rt') as f:
+    readme = f.read()
+long_description = re.sub(
+    r'##### version \d+\.[\d\.]+[a-z]?',
+    '##### version ' + __version__,
+    readme,
+)
+with open('README.md', 'wt') as f:
+    f.write(long_description)
+
 
 print('VERSION:', __version__)
 
-with open('README.md', 'rt') as f:
-    long_description = f.read()
-
 setup(
-    name='steelconnection',
-    install_requires=['requests>=2.12.1'],
+    name=__title__,
+    install_requires=install_requires,
     version=__version__,
-    author='Greg Mueller',
-    author_email='steelconnection@grelleum.com',
-    description='Simplify access to the Riverbed SteelConnect REST API.',
+    author=__author__,
+    author_email=__author_email__,
+    description=__description__,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/grelleum/SteelConnection',
-    keywords=['SteelConnect', 'REST', 'API', 'Riverbed', 'Grelleum'],
-    packages=['steelconnection'],
-    license='MIT',
-    classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent'
-    ],
+    long_description_content_type=content_type,
+    url=__url__,
+    keywords=keywords,
+    packages=packages,
+    license=__license__,
+    classifiers=classifiers,
 )
