581e0d5544d971bcd76360a1a7da3bb586f5e837
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-01 12:21:27 -0400

    completely simplified to fix dependency issue

--- setup.py
+++ setup.py
@@ -1,53 +1,30 @@
-import re
-import setuptools
-
-from steelconnection.__version__ import __author__, __author_email__
-from steelconnection.__version__ import __copyright__, __description__
-from steelconnection.__version__ import __license__, __title__
-from steelconnection.__version__ import __url__, __version__
-
-
-keywords = ['SteelConnect', 'REST', 'API', 'Riverbed', 'Grelleum']
-base_url = 'https://github.com/grelleum/SteelConnection/archive/'
-
-classifiers=(
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent'
-)
+from setuptools import setup
 
 with open('README.md', 'rt') as f:
-    readme = f.read()
+    long_description = f.read()
 
-long_description = re.sub(
-    r'##### version \d+\.\d+\.\d+',
-    '##### version ' + __version__,
-    readme,
-)
-
-if readme != long_description:
-    with open('README.md', 'wt') as f:
-        f.write(long_description)
-
-download_url = base_url + __version__ + '.tar.gz'
-
-packages = setuptools.find_packages()
-
-setuptools.setup(
-    name=__title__,
-    version=__version__,
-    author=__author__,
-    author_email=__author_email__,
-    description=__description__,
+setup(
+    name='steelconnection',
+    install_requires=['requests>=2.12.1'],
+    version='0.9.9',
+    author='Greg Mueller',
+    author_email='steelconnection@grelleum.com',
+    description='Simplify access to the Riverbed SteelConnect REST API.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url=__url__,
-    download_url=download_url,
-    keywords=keywords,
-    packages=packages,
-    install_requires=[
-        'requests',
+    url='https://github.com/grelleum/SteelConnection',
+    keywords=['SteelConnect', 'REST', 'API', 'Riverbed', 'Grelleum'],
+    packages=['steelconnection'],
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
     ],
-    classifiers=classifiers
 )
