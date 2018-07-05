64a236b825a06de09ff795160517011de7903e9f
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-05 17:43:04 -0400

    moved helper function tests to separate file.

--- tests/unit/test_steelconnection.py
+++ tests/unit/test_steelconnection.py
@@ -388,17 +388,3 @@ def test_scon_repr(monkeypatch):
     ).format(realm, scm_version, api_version, pkg_version)
     sc = steelconnection.SConAPI(realm, api_version=api_version)
     assert repr(sc) == expected
-
-
-# Helper Functions:
-
-def test_error_string():
-    """Test _error_string generates a properly formatted string."""
-    url = 'MYREALM'
-    status_code = 600
-    response = fake_requests.Fake_Response(url, status_code, {'no': 'data'})
-    expected = "{0} - Failed\nURL: {1}\nData Sent: {2}".format(
-        status_code, url, repr(response.text)
-    )
-    error = steelconnection.steelconnection._error_string(response)
-    assert error == expected
