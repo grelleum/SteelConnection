0e81ff793a3970e2a5390e30b98344d84cdda232
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-05 17:35:30 -0400

    improved tests for classes that bypass exceptions

--- tests/unit/test_alternate_classes.py
+++ tests/unit/test_alternate_classes.py
@@ -1,7 +1,5 @@
 # coding: utf-8
 
-import getpass
-import json
 import sys
 import pytest
 import requests
@@ -16,12 +14,21 @@ def test_raise_exception_without_exceptions(monkeypatch):
     monkeypatch.setattr(requests, 'Session', fake_requests.Fake_Session)
     sc = steelconnection.SConWithoutExceptions('some.realm')
     response = fake_requests.Fake_Response('', 502, {})
-    sc._raise_exception(response) == None
+    try:
+        result = sc._raise_exception(response)
+    except:
+        assert False
+    else:
+        assert result == None
 
-def test_raise_exception_when_exit_on_error(monkeypatch):
-    """_raise_exception should raise the correct exceptions based on status."""
+
+def test_exit_when_raise_exception_with_exit_on_error(capsys, monkeypatch):
     monkeypatch.setattr(requests, 'Session', fake_requests.Fake_Session)
-    monkeypatch.setattr('sys.exit', lambda x: 'EXIT')
     sc = steelconnection.SConExitOnError('some.realm')
     response = fake_requests.Fake_Response('', 502, {})
-    sc._raise_exception(response) == None
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        result = sc._raise_exception(response)
+    captured = capsys.readouterr()
+    assert captured.err == "502 - Failed\nURL: \nData Sent: '{}'\n"
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 1
