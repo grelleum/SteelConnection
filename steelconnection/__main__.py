c0fa4d9ad784ff3247d95b1cbf39365af9b03c29
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-05 16:32:21 -0400

    remove coverage exclusion

--- steelconnection/__main__.py
+++ steelconnection/__main__.py
@@ -3,6 +3,6 @@ from __future__ import print_function
 from .__version__ import __url__, __version__
 
 
-if __name__ == '__main__':  # pragma no cover
+if __name__ == '__main__':
     print('SteelConnection version:', __version__)
     print('Project home:', __url__)
