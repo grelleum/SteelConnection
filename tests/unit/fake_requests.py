da533796036f65afa8286cdc9c30cf2cb0077129
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-06 17:49:49 -0400

    added auth into mocked reponse to allow validation

--- tests/unit/fake_requests.py
+++ tests/unit/fake_requests.py
@@ -48,7 +48,7 @@ class Fake_Request(object):
 
 
 class Fake_Response(object):
-    def __init__(self, url, status_code, data, content='json'):
+    def __init__(self, url, status_code, data, auth=None, content='json'):
         self.data = json.loads(data) if isinstance(data, str) else data
         self.url = url
         self.ok = True if status_code < 300 else False
@@ -57,6 +57,7 @@ class Fake_Response(object):
         self.headers= {'Content-Type': 'application/' + content}
         self.text = json.dumps(self.data, indent=4)
         self.request = Fake_Request(url, data)
+        self.auth = auth
     def json(self):
         return json.loads(self.text)
 
@@ -71,7 +72,7 @@ class Fake_Session(object):
         if resource == 'netrc401' and auth:
                 resource = 'netrc'
         status_code = codes.get(resource, 200)
-        return Fake_Response(url, status_code, data)
+        return Fake_Response(url, status_code, data, auth)
 
     def getstatus(self, url, auth=None, headers=None, params=None, data=None):
         if data is not None:
@@ -79,13 +80,13 @@ class Fake_Session(object):
         resource = url.split('/')[-1]
         data = responses.get(resource, {})
         status_code = codes.get(resource, 200)
-        return Fake_Response(url, status_code, data)
+        return Fake_Response(url, status_code, data, auth)
 
     def delete(self, url, auth=None, headers=None, params=None, data=None):
         resource = url.split('/')[-1]
         data = responses.get(resource, {}) if not data else data
         status_code = codes.get(resource, 200)
-        return Fake_Response(url, status_code, data)
+        return Fake_Response(url, status_code, data, auth)
 
     def post(self, url, auth=None, headers=None, params=None, data=None):
         if params is not None:
@@ -94,11 +95,11 @@ class Fake_Session(object):
             raise ValueError('post have must data')
         resource = url.split('/')[-1]
         status_code = codes.get(resource, 200)
-        return Fake_Response(url, status_code, data)
+        return Fake_Response(url, status_code, data, auth)
 
     def put(self, url, auth=None, headers=None, params=None, data=None):
         if data is None:
             raise ValueError('put method must have data')
         resource = url.split('/')[-1]
         status_code = codes.get(resource, 200)
-        return Fake_Response(url, status_code, data)
+        return Fake_Response(url, status_code, data, auth)
