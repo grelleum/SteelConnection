5d8df4afa651e78ef07710a305eb8340fb20bdc5
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-07 00:27:04 -0400

    added additional assertions to each test

--- tests/func/test_connection.py
+++ tests/func/test_connection.py
@@ -14,25 +14,45 @@ from PRIVATE import REALM_2_8, REALM_2_9, REALM_2_10, REALM_2_11
 
 def test_create_object_2_8():
     sc = steelconnection.SConAPI(REALM_2_8, REALM_ADMIN, PASSWORD)
-    sc.get('orgs')
+    orgs = sc.get('orgs')
+    assert orgs
+    assert isinstance(orgs, list)
     assert isinstance(sc, steelconnection.SConAPI)
 
 
 def test_create_object_2_9():
     sc = steelconnection.SConAPI(REALM_2_9, REALM_ADMIN, PASSWORD)
-    sc.get('status')
+    orgs = sc.get('orgs')
+    status = sc.get('status')
+    assert orgs
+    assert isinstance(orgs, list)
+    assert status
+    assert isinstance(status, dict)
+    assert 'scm_version' in status
     assert isinstance(sc, steelconnection.SConAPI)
 
 
 def test_create_object_2_10():
     sc = steelconnection.SConAPI(REALM_2_10, REALM_ADMIN, PASSWORD)
-    sc.get('status')
+    orgs = sc.get('orgs')
+    status = sc.get('status')
+    assert orgs
+    assert isinstance(orgs, list)
+    assert status
+    assert isinstance(status, dict)
+    assert 'scm_version' in status
     assert isinstance(sc, steelconnection.SConAPI)
 
 
 def test_create_object_2_11():
     sc = steelconnection.SConAPI(REALM_2_11, REALM_ADMIN, PASSWORD)
-    sc.get('status')
+    orgs = sc.get('orgs')
+    status = sc.get('status')
+    assert orgs
+    assert isinstance(orgs, list)
+    assert status
+    assert isinstance(status, dict)
+    assert 'scm_version' in status
     assert isinstance(sc, steelconnection.SConAPI)
 
 
