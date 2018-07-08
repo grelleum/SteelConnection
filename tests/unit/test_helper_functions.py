01ec565c3a3e607be57615f659196a1bd5c8bb05
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-07 20:47:15 -0400

    fixed repr(json.dumps(data))

--- tests/unit/test_helper_functions.py
+++ tests/unit/test_helper_functions.py
@@ -15,7 +15,7 @@ def test_error_string():
     data = {'no': 'data'}
     response = fake_requests.Fake_Response(url, status_code, data)
     expected = "{0} - Failed\nURL: {1}\nData Sent: {2}".format(
-        status_code, url, repr(json.dumps(data, indent=4))
+        status_code, url, repr(json.dumps(data))
     )
     error = steelconnection.steelconnection._error_string(response)
     assert error == expected
