abd6119851b2c8659eb9e009212d14756f62423f
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-08 19:23:39 -0400

    swapped import PRIVATE for os.environ.get

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -2,23 +2,27 @@
 
 """
 This is a set of pytest functions intended to validate the examples.
-This relies on a PRIVATE import (not supplied) containing personalized details.
+This relies on a environment variables to complete.
 """
 
+import os
 import subprocess
 import sys
 import pytest
 import steelconnection
 
 
-from PRIVATE import REALM_ADMIN, PASSWORD, ORG, REALM
+username = os.environ.get('SCONUSER')
+password = os.environ.get('SCONPASSWD')
+realm = os.environ.get('SCONREALM')
+org_name = os.environ.get('SCONORG')
 
 
 # examples/set_node_location.py
 
 def test_clear_location_fields():
-    sc = steelconnection.SConAPI(REALM, REALM_ADMIN, PASSWORD)
-    org_id, _ = sc.lookup.org(ORG)
+    sc = steelconnection.SConAPI(realm, username, password)
+    org_id, _ = sc.lookup.org(org_name)
     assert org_id
     nodes = sc.get('org/' + org_id + '/nodes')
     assert nodes
@@ -31,15 +35,15 @@ def test_clear_location_fields():
 def test_set_node_location():
     script = 'examples/set_node_location.py'
     command = 'python "{}" "{}" "{}" -u="{}" -p="{}"'.format(
-        script, REALM, ORG, REALM_ADMIN, PASSWORD,
+        script, realm, org_name, username, password,
     )
     output = subprocess.check_output(command, shell=True)
     assert output
 
 
 def test_populated_location_fields():
-    sc = steelconnection.SConAPI(REALM, REALM_ADMIN, PASSWORD)
-    org_id, _ = sc.lookup.org(ORG)
+    sc = steelconnection.SConAPI(realm, username, password)
+    org_id, _ = sc.lookup.org(org_name)
     nodes =  sc.get('org/' + org_id + '/nodes')
     for node in nodes:
         assert node['location'] is not None
@@ -49,13 +53,13 @@ def test_populated_location_fields():
 
 def test_create_site(monkeypatch):
     if sys.version_info.major < 3:
-        monkeypatch.setattr('__builtin__.raw_input', lambda x: REALM_ADMIN)
+        monkeypatch.setattr('__builtin__.raw_input', lambda x: username)
     else:
-        monkeypatch.setattr('builtins.input', lambda x: REALM_ADMIN)
-    monkeypatch.setattr('getpass.getpass', lambda x: PASSWORD)
+        monkeypatch.setattr('builtins.input', lambda x: username)
+    monkeypatch.setattr('getpass.getpass', lambda x: password)
     import create_site
-    create_site.scm_name = REALM
-    create_site.org_name = ORG
+    create_site.scm_name = realm
+    create_site.org_name = org_name
     create_site.new_site = {
         'name': 'pytest',
         'longname': 'pytest',
@@ -64,8 +68,8 @@ def test_create_site(monkeypatch):
         'timezone': 'America/New_York',
     }
     create_site.main()
-    sc = steelconnection.SConAPI(REALM, REALM_ADMIN, PASSWORD)
-    org_id, _ = sc.lookup.org(ORG)
+    sc = steelconnection.SConAPI(realm, username, password)
+    org_id, _ = sc.lookup.org(org_name)
     site_id, site = sc.lookup.site(create_site.new_site['name'], org_id)
     assert site_id
     for key in create_site.new_site:
@@ -73,4 +77,6 @@ def test_create_site(monkeypatch):
     sc.delete('site/' + site_id)
     site_id, _ = sc.lookup.site(create_site.new_site['name'], org_id)
     assert site_id is None
+
+
 
