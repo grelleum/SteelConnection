71d90fc88dd6c01d6579a09522e58fe653a408f8
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-07 20:19:32 -0400

    fixed test_error_string

--- tests/unit/test_helper_functions.py
+++ tests/unit/test_helper_functions.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import json
 import pytest
 import steelconnection
 import fake_requests
@@ -11,9 +12,22 @@ def test_error_string():
     """Test _error_string generates a properly formatted string."""
     url = 'MYREALM'
     status_code = 600
-    response = fake_requests.Fake_Response(url, status_code, {'no': 'data'})
+    data = {'no': 'data'}
+    response = fake_requests.Fake_Response(url, status_code, data)
     expected = "{0} - Failed\nURL: {1}\nData Sent: {2}".format(
-        status_code, url, repr(response.text)
+        status_code, url, repr(json.dumps(data, indent=4))
     )
     error = steelconnection.steelconnection._error_string(response)
     assert error == expected
+
+
+# def test_error_string_bad_input():
+#     """Test _error_string generates a ValueError Exception."""
+#     with pytest.raises(ValueError):
+#         response = fake_requests.Fake_Response(
+#             'old.sch.ool/status',
+#             600,
+#             '<!DOCTYPE html>\n\n\n<html>\n'
+#         )
+#     with pytest.raises(UnboundLocalError):
+#         error = steelconnection.steelconnection._error_string(response)
