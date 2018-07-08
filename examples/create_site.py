d307eacc276f822646fec3c9de557b0940b4f352
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-08 13:47:42 -0400

    if __name__ == '__main__':

--- examples/create_site.py
+++ examples/create_site.py
@@ -14,19 +14,24 @@ new_site = {
     'timezone': 'America/New_York',
 }
 
-# Initialize the steelconnection object.
-sc = steelconnection.SConAPI(scm_name)
+def main():
+    # Initialize the steelconnection object.
+    sc = steelconnection.SConAPI(scm_name)
 
-# Get the org ID for your organization.
-org_id, org = sc.lookup.org(org_name)
-print('Org name: {},  org_id: {}'.format(org_name, org_id))
+    # Get the org ID for your organization.
+    org_id, org = sc.lookup.org(org_name)
+    print('Org name: {},  org_id: {}'.format(org_name, org_id))
 
-# API resource for posting.
-resource = '/org/{}/sites'.format(org_id)
+    # API resource for posting.
+    resource = '/org/{}/sites'.format(org_id)
 
-# Make the post request.
-result = sc.post(resource, data=new_site)
+    # Make the post request.
+    result = sc.post(resource, data=new_site)
 
-# Display response.
-print('Response:', sc.response.status_code, sc.response.reason)
-print(result)
+    # Display response.
+    print('Response:', sc.response.status_code, sc.response.reason)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
