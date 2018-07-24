c6ed4b718aeaf7ce8c0914037ea446ce06be11af
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-24 17:34:01 -0400

    include check fro python version

--- tests/func/test_import.py
+++ tests/func/test_import.py
@@ -3,10 +3,11 @@
 
 import pytest
 import subprocess
+import sys
 import steelconnection
 
 
-def test_import_dumder_all():
+def test_import_dunder_all():
     expected = set((
         'SConAPI', 'SConWithoutExceptions', 'SConExitOnError',
         'AuthenticationError', 'APINotEnabled', 
@@ -19,7 +20,8 @@ def test_import_dumder_all():
 def test_import_on_command_line():
     output = subprocess.check_output('python -m steelconnection', shell=True)
     output = output.decode()
-    expected = u'SteelConnection version: {0}\nProject home: {1}\n'.format(
+    expected = u'Python version: {0}\nSteelConnection version: {1}\nProject home: {2}\n'.format(
+        '.'.join(str(x) for x in sys.version_info[:3]),
         steelconnection.__version__,
         steelconnection.__url__,
     )
