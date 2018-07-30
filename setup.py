db1043d30111a52eea1f7599aca4277c4955e023
Greg Mueller <greg.p.mueller@gmail.com>
2018-07-30 18:16:54 -0400

    version bumped to 0.9.29

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import re
 
 name = 'steelconnection'
 description = 'Simplify access to the Riverbed SteelConnect REST API.'
-version = '0.9.28'
+version = '0.9.29'
 copyright = 'Copyright 2018 Greg Mueller'
 
 info = {
