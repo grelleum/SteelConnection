b437985a7846843e521428421c3cd99616167004
Greg Mueller <greg.p.mueller@gmail.com>
2018-08-05 09:29:06 -0400

    Added ascii art.

--- examples/set_node_location.py
+++ examples/set_node_location.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+# coding: utf-8
+
+#    ______          _______                       __  _
+#   / __/ /____ ___ / / ___/__  ___  ___  ___ ____/ /_(_)__  ___
+#  _\ \/ __/ -_) -_) / /__/ _ \/ _ \/ _ \/ -_) __/ __/ / _ \/ _ \
+# /___/\__/\__/\__/_/\___/\___/_//_/_//_/\__/\__/\__/_/\___/_//_/
+#
+#
+# SteelConnection
+# Simplify access to the Riverbed SteelConnect REST API.
+#
+# https://pypi.org/project/steelconnection
+# https://github.com/grelleum/SteelConnection
+
 
 """Update SteelConnect nodes within a specified Org
 by copying the site name to the location field
