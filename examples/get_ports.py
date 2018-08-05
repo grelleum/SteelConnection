35a46db90d8a20f7b23f2e98812cab5946c80d07
Greg Mueller <greg.p.mueller@gmail.com>
2018-08-05 09:36:42 -0400

    Wrapped with manin() function.

--- examples/get_ports.py
+++ examples/get_ports.py
@@ -20,29 +20,30 @@ from __future__ import print_function
 import steelconnection
 
 
-appliance = steelconnection.get_input(
-    'Enter the appliance serial number: '
-)
+def main():
+    sc = steelconnection.SConnect()
 
-print("Note: SteelConnect Realm is usually in the form 'realm.riverbed.cc'")
-sc = steelconnection.SConnect()
-node = sc.lookup.node(appliance)
+    appliance = steelconnection.get_input('Enter appliance serial number: ')
+    node = sc.lookup.node(appliance)
 
-ports = sc.get('node/' + node['id'] + '/ports')
+    ports = sc.get('node/' + node['id'] + '/ports')
 
-line = '{:14}{:10}{:8}{:8}{:8}'
-print(line.format('\nPort ID', 'ifname', 'Link', 'Speed', 'Duplex'))
-print(line.format('-------', '------', '----', '-----', '------'))
+    line = '{:14}{:10}{:8}{:8}{:8}'
+    print(line.format('\nPort ID', 'ifname', 'Link', 'Speed', 'Duplex'))
+    print(line.format('-------', '------', '----', '-----', '------'))
 
-for port in ports:
-    resource = 'port/{}'.format(port['id'])
-    port_status = sc.getstatus(resource)
-    print(line.format(
-        port['port_id'],
-        port['ifname'],
-        'UP' if port_status['link'] else 'DOWN',
-        port_status['speed'],
-        port_status['duplex'],
-    ))
+    for port in ports:
+        resource = 'port/{}'.format(port['id'])
+        port_status = sc.getstatus(resource)
+        print(line.format(
+            port['port_id'],
+            port['ifname'],
+            'UP' if port_status['link'] else 'DOWN',
+            port_status['speed'],
+            port_status['duplex'],
+        ))
+    print()
 
-print()
+
+if __name__ == '__main__':
+    main()
