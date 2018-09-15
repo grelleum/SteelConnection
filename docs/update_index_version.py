6e30581d825438a27e2d4b82134f717387c46dce
Greg Mueller <greg.p.mueller@gmail.com>
2018-09-15 13:52:58 -0500

    Removed apostrophes.

--- docs/update_index_version.py
+++ docs/update_index_version.py
@@ -17,10 +17,9 @@ def get_version(filename):
     with open(filename, 'rt') as f:
         for line in f:
             if line.startswith('version'):
-                return line.split()[-1]
+                return line.split()[-1].replace("'", '')
 
 
 version = get_version('../setup.py')
 print('VERSION:', version)
 update_version(version, 'index.rst')
-
