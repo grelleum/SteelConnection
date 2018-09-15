dcff4f0167ddf4aea5a736564b44bbc317f40ac0
Greg Mueller <greg.p.mueller@gmail.com>
2018-09-15 18:00:31 -0500

    Version 0.94.0.

--- steelconnection/__version__.py
+++ steelconnection/__version__.py
@@ -3,7 +3,7 @@
 
 __title__ = 'steelconnection'
 __description__ = 'Simplify access to the Riverbed SteelConnect REST API.'
-__version__ = '0.93.1'
+__version__ = '0.94.0'
 __author__ = 'Greg Mueller'
 __author_email__ = 'steelconnection@grelleum.com'
 __copyright__ = 'Copyright 2018 Greg Mueller'
