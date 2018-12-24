43d5f3a54fb3d999f399c25ff07d46df254d2744
grelleum <greg.p.mueller@gmail.com>
2018-12-24 11:18:21 -0500

    removed commented code.

--- steelconnection/api.py
+++ steelconnection/api.py
@@ -436,27 +436,6 @@ class SConnect(object):
             self.response.status_code, self.response.reason, repr(error_message)
         )
 
-    # @property
-    # def recv(self):
-    #     """Return summary of the previous API response.
-
-    #     :returns: Details regarding previous API request.
-    #     :rtype: str
-    #     """
-    #     error = None
-    #     if self.response.text and not self.response.ok:
-    #         try:
-    #             details = self.response.json()
-    #             error = details.get("error", {}).get("message")
-    #         except ValueError:  # bad json
-    #             pass
-    #         except AttributeError:  # non-dict json
-    #             pass
-    #     error = ",  Error: " + repr(error) if error else ""
-    #     return "RECV: {} - {}{}".format(
-    #         self.response.status_code, self.response.reason, error
-    #     )
-
     # Error handling and Exception generation.
 
     def _exception_handling(self, on_error):
