9af47366cc833f054b36595f1465cf9bce28f67f
Greg Mueller <greg.p.mueller@gmail.com>
2019-01-01 09:46:59 -0500

    Version 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = "2018, Greg Mueller"
 author = "Greg Mueller"
 
 # The short X.Y version
-version = "0.9.3"
+version = "1.0.0"
 # The full version, including alpha/beta/rc tags
-release = "0.9.3"
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
