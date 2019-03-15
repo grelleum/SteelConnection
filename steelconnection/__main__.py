dffa282d0c2a1e49d2f57a6c4d0efa72e0348f9d
Greg Mueller <greg.p.mueller@gmail.com>
2019-03-15 17:17:05 -0400

    Added  documentation to __main__.

--- steelconnection/__main__.py
+++ steelconnection/__main__.py
@@ -7,11 +7,13 @@ from __future__ import print_function
 
 import sys
 
-from .__version__ import __url__, __version__
+from .__version__ import __documentation__, __url__, __version__
 from .api import ASCII_ART
 
 if __name__ == "__main__":
     print(ASCII_ART)
     print("Python version:", ".".join(str(x) for x in sys.version_info[:3]))
     print("SteelConnection version:", __version__)
-    print("Project home:", __url__)
+    print("Documentation:", __documentation__)
+    print("Project home: ", __url__)
+    print()
